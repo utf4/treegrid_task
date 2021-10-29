0b228f80189344e4519304d8a6504a4353fc05c0
utf4 <furqan@invozone.com>
2021-10-29 17:06:11 +0500

    feat: treegrid implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild, Inject} from '@angular/core';
 import { sampleData } from '../jsontreegriddata';
-import { TreeGridComponent, RowDDService,SelectionService,ResizeService, PageService,ExcelExportService, PdfExportService, ColumnChooserService, ToolbarService,EditService, FilterService,SortService, Column, ContextMenuService} from '@syncfusion/ej2-angular-treegrid';
+import {  TreeGridComponent, RowDDService,SelectionService, ResizeService,
+          PageService,ExcelExportService, PdfExportService, ColumnChooserService,
+          ToolbarService,EditService, FilterService,SortService, Column,
+          ContextMenuService} from '@syncfusion/ej2-angular-treegrid';
 import {PageSettingsModel } from '@syncfusion/ej2-angular-grids';
-import { removeClass, addClass } from '@syncfusion/ej2-base';
 import { DropDownList, ChangeEventArgs } from '@syncfusion/ej2-angular-dropdowns';
-import { DropDownListComponent } from '@syncfusion/ej2-angular-dropdowns';
-import { SortEventArgs } from '@syncfusion/ej2-grids';
-import { CheckBoxComponent } from '@syncfusion/ej2-angular-buttons';
-import { CheckBoxAllModule} from '@syncfusion/ej2-angular-buttons';
-import { ActionEventArgs } from '@syncfusion/ej2-grids';
 import { ReorderService, EditSettingsModel } from '@syncfusion/ej2-angular-treegrid';
-import { MenuEventArgs } from '@syncfusion/ej2-navigations';
-import { BeforeOpenCloseEventArgs } from '@syncfusion/ej2-inputs';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { ContextMenuClickEventArgs } from '@syncfusion/ej2-grids';
-
-
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -36,7 +27,8 @@ import { ContextMenuClickEventArgs } from '@syncfusion/ej2-grids';
     ContextMenuService,
     FilterService,
     ReorderService,
-  ]
+  ],
+  encapsulation: ViewEncapsulation.None,
 
 })
 export class AppComponent  implements OnInit {  
@@ -55,40 +47,40 @@ export class AppComponent  implements OnInit {
   public ddlfields: Object | undefined;
   public d2data: Object | undefined;
   public fields: Object | undefined;
-  public columns: any;
   public selectOptions: Object | undefined;
   public editparams: Object | undefined;
   public rowIndex: any;
   public cellIndex: any;
   public selectionOptions: Object | undefined;
   public row : Number | undefined;
+  public modalTitle ='Add Column';
+  public modalData = {id: '',field: '',headerText:'',width: '70', textAlign:'' };
+  public columns: Array<{field: string, headerText: string, width: string, textAlign: string, format?:string}> = [];
+  public seleted_rows : any | undefined;
+  public selected_row_index : any | undefined;
+  public is_cut : Boolean | undefined;
 
-
+  @ViewChild('content', { static: false }) private content: any;
   @ViewChild('treegrid')
   public treegrid: TreeGridComponent;
 
-
+  constructor(private modalService: NgbModal) {
+   
+  }
   ngOnInit(): void {
     
     this.data = sampleData;
-    this.columns = [
-      { 'field': "taskID", 'headerText':"Task ID" , 'width': '70' , 'textAlign':'Right'},
-      { 'field': "taskName", 'headerText':"Task Name", 'width': '200' , 'textAlign':''},
-      { 'field': "startDate",'headerText':"Start Date", 'width': '90' , 'textAlign':'Right', 'format':'yMd'},
-      { 'field': "endDate", 'headerText':"End Date", 'width': '90' , 'textAlign':'Right', 'format':'yMd'},
-      { 'field': "duration",'headerText':"Duration", 'width': '80' , 'textAlign':'Right'},
-      { 'field': "progress",'headerText':"Progress", 'width': '80' , 'textAlign':'Right' },
-      { 'field': "priority",'headerText':"Priority", 'width': '90' , 'textAlign':''},
-
-    ];
+    this.columns = this.ColumnVaue();
     this.pageSettings = { pageSize: 10 };
     this.editing = { allowDeleting: true, allowEditing: true, mode: 'Row' };
     this.contextMenuItems= [
-      // { text: 'Add Column', target: '.e-headercontent', id: 'create-column' },
-      // { text: 'Edit', target: '.e-headercontent', id: 'edit-column' },
+      { text: 'Add Column', target: '.e-headercontent', id: 'create-column' },
+      { text: 'Edit', target: '.e-headercontent', id: 'edit-column' },
+      { text: 'Delete', target: '.e-headercontent', id: 'delete-column' },
       { text: 'Copy', target:'.e-content', id: 'customCopy' },
-      { text: 'Paste', target: '.e-content', id: 'customPaste' },
-      { text: 'Delete', target: '.e-headercontent', id: 'delete-column' }
+      { text: 'Cut', target: '.e-content', id: 'customCut' },
+      { text: 'PasteNext', target: '.e-content', id: 'customPasteNext' },
+      { text: 'PasteChild', target: '.e-content', id: 'customPasteChild' },
     ]
     this.toolbar = ['ColumnChooser'];
     this.filterSettings = { type: 'FilterBar', hierarchyMode: 'Parent', mode: 'Immediate' };
@@ -132,30 +124,125 @@ export class AppComponent  implements OnInit {
     (this.pageSettings = { pageSize: 10 });
     this.editparams = { params: { format: 'n' } };
   }
+
+  ColumnVaue(){
+    return [       
+      { field : "taskID", headerText:"Task ID" , width: '70' , textAlign:'Right',format:''},
+      { field: "taskName", headerText:"Task Name", width: '200' , textAlign:'' ,format:''},
+      { field: "startDate",headerText:"Start Date", width: '90' , textAlign:'Right', format:'yMd'},
+      { field: "endDate", headerText:"End Date", width: '90' , textAlign:'Right', format:'yMd'},
+      { field: "duration",headerText:"Duration", width: '80' , textAlign:'Right',format:''},
+      { field: "progress",headerText:"Progress", width: '80' , textAlign:'Right',format:'' },
+      { field: "priority",headerText:"Priority", width: '90' , textAlign:'',format:''},
+      ]
+  }
   contextMenuOpen(args : any): void {
     this.rowIndex = args.rowInfo.rowIndex;
     this.cellIndex = args.rowInfo.cellIndex;
   }
-  
+  customCopy(){
+    this.row=this.rowIndex;
+    let rows=this.treegrid.getSelectedRecords(); 
+    this.seleted_rows=rows;
+  }
+  customCut(){
+    this.row=this.rowIndex;
+    this.is_cut=true;
+    let rows=this.treegrid.getSelectedRecords(); 
+    let rows_index=this.treegrid.getSelectedRowIndexes(); 
+    this.selected_row_index=rows_index;
+    this.seleted_rows=rows;
+  }
+  customPasteNext(){
+    let tem_arr=this.data;
+    tem_arr=tem_arr.concat(this.seleted_rows);
+     if(this.is_cut==true){
+        for(let i=0;i<this.selected_row_index.length;i++){
+        delete tem_arr[this.selected_row_index[i]]
+      }
+      tem_arr = tem_arr.filter(item => item != undefined);
+    }
+     this.treegrid.dataSource = JSON.parse(JSON.stringify(tem_arr));
+     this.is_cut=false;
+     this.seleted_rows=[];
+     this.selected_row_index=[]
+  }
+  customPasteChild(){
+    let tem_arr=this.data;
+    let array_obj: any =tem_arr[Number(this.rowIndex)];
+    let new_array=array_obj.subtasks.concat(this.seleted_rows);
+    array_obj.subtasks=new_array;
+    tem_arr[this.rowIndex]=array_obj;
+    if(this.is_cut==true){
+      for(let i=0;i<this.selected_row_index.length;i++){
+        delete tem_arr[this.selected_row_index[i]]
+      }
+      tem_arr = tem_arr.filter(item => item != undefined);
+    }
+    this.treegrid.dataSource = JSON.parse(JSON.stringify(tem_arr));
+    this.is_cut=false;
+    this.seleted_rows=[];
+    this.selected_row_index=[];
+  }
   contextMenuClick (args?: any): void {
-  
     if (args.item.id == 'customCopy') {
-      this.treegrid.copy();
-      this.row=this.rowIndex;
-    } else if (args.item.id == 'customPaste') {
-      var rowIndex = this.rowIndex;
-      var cellIndex = this.cellIndex;
-       let tem_arr=[];
-       this.data.push(this.data[Number(this.row)]);
-       tem_arr=this.data;
-       this.treegrid.dataSource = JSON.parse(JSON.stringify(tem_arr));
+      this.customCopy();
+    }else if(args.item.id == 'customCut'){
+      this.customCut();
+    }else if (args.item.id == 'customPasteNext') {
+     this.customPasteNext();
+    }else if(args.item.id =='customPasteChild'){
+      this.customPasteChild();
     }else if (args.item.id === 'create-column') {
-
+      this.addColumn();
     }else if (args.item.id === 'edit-column'){
-      console.log(args, args?.column.index, 'here');
+      this.editColumn(args?.column.index);
     }else if (args.item.id === 'delete-column'){
       this.columns.splice(args?.column.index, 1);
     }
   }
+  open(content:any) {
+    this.modalService.open(content);
+  }
+  addColumn(){
+    this.modalTitle = 'Add Column';
+    this.setModalDefaultValue();
+    this.open(this.content);
+  }
+  editColumn(columnIndex:any){
+    this.modalTitle = 'Edit Column';
+    let data = this.columns[columnIndex];
+    this.modalData = {
+      id: columnIndex,
+      field: data.field,
+      headerText: data.headerText,
+      width: data.width,  
+      textAlign:data.textAlign
+    }
+    this.open(this.content);
+  }
+  setModalDefaultValue(){
+    this.modalData = {
+      id: '',
+      field: '',
+      headerText:'',
+      width: '70', 
+      textAlign:''
+    }
+  }
+  saveData(){
+    if(this.modalData.headerText === '' || this.modalData.field === ''){
+      alert('Plase enter Data');
+    }else if(this.modalData.id !== ''){
+      let data : any =  this.ColumnVaue()
+      data[this.modalData.id].headerText = 'asdasda';
+      this.columns = [...data];
+    }else{
+      this.columns.push(this.modalData)
+      this.setModalDefaultValue();
+      this.modalService.dismissAll();
+    }
+  }
+ 
 }
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,TreeGridModule
+    BrowserModule,TreeGridModule,NgbModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
